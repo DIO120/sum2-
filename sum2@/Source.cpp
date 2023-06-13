60c39075db19804e7c6184287862ea3095a5c859
1 <1@O-122>
2023-06-13 20:11:29 +0300

    3

--- sum2@/Source.cpp
+++ sum2@/Source.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
+double sum(int a, int b) {
+	return a + b;
+}
 int main() {
-	int a = 8, b = 9;
-	cout << a + b << endl;
+	int a, b;
+	cin >> a >> b;
+	cout << sum(a, b) << endl;
+
 	return 0;
 }
